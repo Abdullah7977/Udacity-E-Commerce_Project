0802f9a088f5464261da8cda7050df577119dcb0
Abdullah7977 <abdobody7777777@gmail.com>
2022-05-12 23:23:47 +0200

    first commit

---
+++ test-output-thread/report.js
@@ -0,0 +1,109 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "e21cc2d2-02c3-4b3c-b689-beec2678d03e",
+    "feature": "The Logged User Operations",
+    "scenario": "SC7- Logged user could filter with color",
+    "start": 1652389587151,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1652389590921,
+    "className": "passed"
+  },
+  {
+    "id": "aba6217e-55dd-4a88-9d21-23d0ac36035a",
+    "feature": "The Logged User Operations",
+    "scenario": "SC5- Logged User could switch between currencies US-Euro",
+    "start": 1652389577436,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1652389583607,
+    "className": "passed"
+  },
+  {
+    "id": "f4c97734-efd2-465e-985e-79b4e9742bb9",
+    "feature": "The Logged User Operations",
+    "scenario": "SC6- Logged user could select different Categories",
+    "start": 1652389583638,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1652389587126,
+    "className": "passed"
+  },
+  {
+    "id": "a81925ab-af46-41bd-ae14-e8fe62117773",
+    "feature": "The Logged User Operations",
+    "scenario": "SC8- Logged user could filter with color",
+    "start": 1652389590976,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1652389592168,
+    "className": "passed"
+  },
+  {
+    "id": "aad0e202-d05e-4e1a-8df7-426f620fbb63",
+    "feature": "The Logged User Operations",
+    "scenario": "SC9- Logged user could add different products to Shopping cart",
+    "start": 1652389592188,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1652389598229,
+    "className": "passed"
+  },
+  {
+    "id": "9fd6e09a-26fd-458b-868e-042047e2427b",
+    "feature": "The Un Logged User Operations",
+    "scenario": "SC1- User could register with valid data",
+    "start": 1652389531961,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1652389566775,
+    "className": "passed"
+  },
+  {
+    "id": "28e87d4f-a511-4287-86b3-edd664eba26f",
+    "feature": "The Un Logged User Operations",
+    "scenario": "SC3- User could log in with valid email and password",
+    "start": 1652389570871,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1652389574699,
+    "className": "passed"
+  },
+  {
+    "id": "304fe533-2c2a-40ff-9e39-2d84f1b13c8a",
+    "feature": "The Un Logged User Operations",
+    "scenario": "SC2- User could reset his/her password successfully",
+    "start": 1652389566832,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1652389570841,
+    "className": "passed"
+  },
+  {
+    "id": "0fbe672e-eb49-446e-a045-863b76959fa7",
+    "feature": "The Logged User Operations",
+    "scenario": "SC4-Logged User could search for any product",
+    "start": 1652389574741,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1652389577404,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 1,
+    "content": "Thread[main,5,main]"
+  }
+]);
+});
